f4d65889ac9c35083564e52135b1b1cd37ba3bd1
NatalaBoiko <natalabojko@gmail.com>
2022-05-06 19:06:16 +0300

    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+console.log(galleryItems);
+
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const makeImages = ({ preview, original, description }) => {
+  return `
+    <a class = 'gallery__item' href = '${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        alt='${description}'
+        />
+      </a>
+    `;
+};
+
+const makeImagesList = galleryItems.map(makeImages).join('');
+galleryEl.insertAdjacentHTML('beforeend', makeImagesList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: false,
+});
