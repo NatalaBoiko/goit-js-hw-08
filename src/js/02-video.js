930d0d46da785ca9392e5fc9ef0f6039adffc8bd
NatalaBoiko <natalabojko@gmail.com>
2022-05-10 14:11:20 +0300

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("iframe");
+const player = new Vimeo.Player(iframe);
+
+const playingTime = (evt) => {
+  localStorage.setItem("videoplayer-current-time", evt.seconds);
+  //   console.log(playingTime);
+  console.log(evt);
+};
+
+player.on("timeupdate", throttle(playingTime, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
